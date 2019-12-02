488eaa32856cd088b1b59a011e0e554a434c231f
lalit184 <lalit184@icloud.com>
2019-12-02 22:25:20 +0530

    training script with f1 score for each label

--- Train.py
+++ Train.py
@@ -100,7 +100,7 @@ for epoch in range(1, epochs):
     predictions = predictions > 0.5 
     predictions = predictions.astype('float').tolist()
     
-    f1.append([f1_score(all_test_labels[:][i], predictions[:][i]) for i in range(6)])
+    f1.append([f1_score(all_test_labels[:][i], predictions[:][i], average='weighted') for i in range(6)])
     print(f1)
 
 plt.plot(train_losses, label='Training Loss', color='blue')
