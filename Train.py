ac10e06790726797739793475d5117e693d3d75d
lalit184 <lalit184@icloud.com>
2019-12-02 21:43:34 +0530

    training script with f1 score for each label

--- Train.py
+++ Train.py
@@ -96,7 +96,7 @@ for epoch in range(1, epochs):
         torch.save(Model.state_dict(), "model_prunned_lr.pt")
         min_loss = l0
     
-    predictions = np.array(predictions, dtype='np.float32') 
+    predictions = np.array(predictions, dtype = np.float32) 
     predictions = predictions > 0.5 
     predictions = predictions.astype('float').tolist()
     
